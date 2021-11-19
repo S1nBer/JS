5a8331e74aae9c740c155c9d51e86c1a471db051
roowix <gerasimenko_andrei@bk.ru>
2021-11-19 16:14:15 +0700

    homework for numbers

---
+++ hw-numbers.js
@@ -0,0 +1,32 @@
+//1
+let value = Math.PI.toFixed(2);
+
+console.log(value);
+
+//2
+value = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+
+console.log(value);
+
+value = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+
+console.log(value);
+
+//3
+value = Math.random().toFixed(2);
+
+console.log(value);
+
+value = Math.round(Math.random() * 5);
+
+console.log(value);
+
+//4
+value = (0.6+0.7).toFixed(1);
+
+console.log(value);
+
+//5
+value = parseInt('100$');
+
+console.log(value);
