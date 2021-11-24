72d66681ef108ce61c45486fc1707eae7731462c
roowix <gerasimenko_andrei@bk.ru>
2021-11-24 12:34:04 +0700

    gomework for objects

---
+++ hw-objects.js
@@ -0,0 +1,13 @@
+//1
+let object = {
+    product: 'iphone',
+};
+
+//2
+object.price = 1000;
+object.currency = 'dollar';
+
+//3
+object.details = {};
+object.details.model = '';
+object.details.color = '';
