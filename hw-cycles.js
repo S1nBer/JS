58281d83597f0dfa037c083cacbfc5efdebe498e
roowix <gerasimenko_andrei@bk.ru>
2021-11-26 11:53:24 +0700

    prettier

--- hw-cycles.js
+++ hw-cycles.js
@@ -1,23 +1,23 @@
 //1
-let str = 'i am in the easycode';
-let res = '';
+let str = "i am in the easycode";
+let res = "";
 
 for (let i = 0; i < str.length; i++) {
-    if(str[i - 1] === ' ' || i === 0) {
-        res += str[i].toUpperCase();
-    } else {
-        res += str[i];
-    }
+  if (str[i - 1] === " " || i === 0) {
+    res += str[i].toUpperCase();
+  } else {
+    res += str[i];
+  }
 }
 
 console.log(res);
 
 //2
-let str1 = 'tseb eht ma i';
-let res1 = '';
+let str1 = "tseb eht ma i";
+let res1 = "";
 
 for (let i = 1; i <= str1.length; i++) {
-    res1 += str1[str1.length - i];
+  res1 += str1[str1.length - i];
 }
 
 console.log(res1);
@@ -27,21 +27,21 @@ let x = 5;
 let fact = 1;
 
 for (let i = 1; i <= x; i++) {
-    fact *= i;
+  fact *= i;
 }
 
 console.log(fact);
 
 //4
-let str2 = 'JavaScript is a pretty good language';
-let res2 = '';
+let str2 = "JavaScript is a pretty good language";
+let res2 = "";
 
 for (let i = 0; i < str2.length; i++) {
-    if (str2[i - 1] === ' ') {
-        res2 += str2[i].toUpperCase();
-    } else if (str2[i] !== ' ') {
-        res2 += str2[i];
-    }
+  if (str2[i - 1] === " ") {
+    res2 += str2[i].toUpperCase();
+  } else if (str2[i] !== " ") {
+    res2 += str2[i];
+  }
 }
 
 console.log(res2);
@@ -50,26 +50,24 @@ console.log(res2);
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2) {
-        console.log(arr[i]);
-    }
+  if (arr[i] % 2) {
+    console.log(arr[i]);
+  }
 }
 
 //6
 let list = {
+  name: "denis",
 
-    name: 'denis',
-    
-    work: 'easycode',
-    
-    age: 29
-    
-}
+  work: "easycode",
+
+  age: 29,
+};
 
 for (let key in list) {
-    if (typeof list[key] === 'string') {
-        list[key] = list[key].toUpperCase();
-    }
+  if (typeof list[key] === "string") {
+    list[key] = list[key].toUpperCase();
+  }
 }
 
 console.log(list);
