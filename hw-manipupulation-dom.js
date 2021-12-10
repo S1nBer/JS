0ae10f02e5f8e197d4a0133be9ef87f6ed821631
roowix <gerasimenko_andrei@bk.ru>
2021-12-10 19:34:03 +0700

    homework for manipulation dom

---
+++ hw-manipupulation-dom.js
@@ -0,0 +1,27 @@
+//1
+const p = document.querySelector("p");
+console.log(p.textContent);
+//2
+function elementInfo(item) {
+  return {
+    type: item.nodeType,
+    name: item.tagName,
+    children: item.childNodes.length,
+  };
+}
+
+console.log(elementInfo(p));
+
+//3
+const ul = document.querySelector("ul");
+const arr = ul.querySelectorAll("a");
+const arrOfContent = [...arr].map((el) => el.textContent);
+console.log(arrOfContent);
+
+//4
+[...p.childNodes].forEach((el) => {
+  if (el.nodeType === 3) {
+    el.textContent = "-text-";
+  }
+});
+console.log(res[0].nodeType);
