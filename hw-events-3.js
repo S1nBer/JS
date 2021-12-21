c9a94af554a5a4e832203782328d78cb99751d05
roowix <gerasimenko_andrei@bk.ru>
2021-12-21 18:39:47 +0700

    homework for events number 3

---
+++ hw-events-3.js
@@ -0,0 +1,43 @@
+const colors = [
+  "red",
+  "blue",
+  "olive",
+  "orange",
+  "pink",
+  "yellow",
+  "green",
+  "gray",
+  "aqua",
+  "brown",
+];
+
+const boxes = document.querySelectorAll(".box");
+[...boxes].forEach((box) => {
+  box.addEventListener("mouseenter", (e) => {
+    e.stopPropagation();
+    let square = box;
+    let allBoxes = [square];
+    while (square.children.length !== 0) {
+      square = square.children[0];
+      allBoxes.push(square);
+    }
+    allBoxes.forEach((el, i) => {
+      setTimeout(setColor, 200 * (1 + i), el);
+    });
+  });
+});
+
+[...boxes].forEach((box) => {
+  box.addEventListener("mouseleave", (e) => {
+    /* e.stopPropagation(); */
+    box.style.removeProperty("background");
+  });
+});
+
+function random() {
+  return Math.floor(Math.random() * 10);
+}
+
+function setColor(el) {
+  el.style.background = colors[random()];
+}
