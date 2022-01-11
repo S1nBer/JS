73e16ba23b940195ca2e0a4b259b7f652afc45c8
roowix <gerasimenko_andrei@bk.ru>
2022-01-11 16:46:53 +0700

    inheritance

--- lc-oop.js
+++ lc-oop.js
@@ -1,5 +1,5 @@
 const str = new String("hello world");
-console.log(str);
+/* console.log(str); */
 
 function Product(brand, price, discount) {
   this.brand = brand;
@@ -18,5 +18,47 @@ Product.prototype.setPrice = function (price) {
 const apple = new Product("apple", 100, 15);
 const samsung = new Product("samsung", 50, 10);
 
-console.log(apple.getPriceWithDiscount());
-console.log(samsung);
+/* console.log(apple.getPriceWithDiscount());
+console.log(samsung); */
+
+const protoForObj = {
+  sayHello() {
+    return "Hello World!";
+  },
+};
+
+const obj = Object.create(protoForObj, {
+  firstName: { value: "Andrei" },
+});
+
+function User(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+User.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+User.prototype.sayHello = function () {
+  return `Hello ${this.firstName} ${this.lastName}`;
+};
+
+const user = new User("Andrei", "Gerasimenko");
+
+// Customer
+function Customer(firstName, lastName, membership) {
+  User.call(this, firstName, lastName);
+
+  this.membership = membership;
+}
+
+Customer.prototype = Object.create(User.prototype);
+// return constructor link
+Customer.prototype.constructor = Customer;
+
+Customer.prototype.getMembership = function () {
+  return this.membership.toUpperCase();
+};
+
+const customer = new Customer("Tobi", "Maquayr", "basic");
