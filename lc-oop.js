0baef452cc03f9f02c93dd5138994b235af4977a
roowix <gerasimenko_andrei@bk.ru>
2022-01-11 16:14:20 +0700

    prototype

--- lc-oop.js
+++ lc-oop.js
@@ -5,12 +5,18 @@ function Product(brand, price, discount) {
   this.brand = brand;
   this.price = price;
   this.discount = discount;
-  this.getPriceWithDiscount = function () {
-    return (this.price * (100 - this.discount)) / 100;
-  };
 }
+
+Product.prototype.getPriceWithDiscount = function () {
+  return (this.price * (100 - this.discount)) / 100;
+};
+
+Product.prototype.setPrice = function (price) {
+  this.price = price;
+};
 
 const apple = new Product("apple", 100, 15);
 const samsung = new Product("samsung", 50, 10);
+
 console.log(apple.getPriceWithDiscount());
 console.log(samsung);
