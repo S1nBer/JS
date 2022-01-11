540942197c8f640b2dfc4475126b0da270884818
roowix <gerasimenko_andrei@bk.ru>
2022-01-11 14:17:47 +0700

    OOP

---
+++ lc-oop.js
@@ -0,0 +1,16 @@
+const str = new String("hello world");
+console.log(str);
+
+function Product(brand, price, discount) {
+  this.brand = brand;
+  this.price = price;
+  this.discount = discount;
+  this.getPriceWithDiscount = function () {
+    return (this.price * (100 - this.discount)) / 100;
+  };
+}
+
+const apple = new Product("apple", 100, 15);
+const samsung = new Product("samsung", 50, 10);
+console.log(apple.getPriceWithDiscount());
+console.log(samsung);
