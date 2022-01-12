dc99d772fe67f30dc722c4ab41f0d20627b5e51a
roowix <gerasimenko_andrei@bk.ru>
2022-01-12 14:52:51 +0700

    modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
-import { conf as c } from './module1';
+/* import { conf as c } from './module1';
 // import * as mod1 from './module1';
 import Product from './module2';
 
 console.log(new Product('Apple'));
 console.log(c);
-// console.log(mod1.foo());
+// console.log(mod1.foo()); */
+
+import * as mod1 from "./module1";
+import Product from "./module2";
+
+console.log(new Product("Jija"));
+mod1.foo();
--- src/js/module1.js
+++ src/js/module1.js
@@ -1,9 +1,19 @@
-const config = {
+/* const config = {
   apiUrl: 'demo.com',
 };
 
 function myFunction() {
   console.log('module1, myFunction');
 }
+
+export { config as conf, myFunction as foo }; */
+
+function myFunction() {
+  console.log("module1, myFunction");
+}
+
+const config = {
+  apiUrl: "demo.com",
+};
 
 export { config as conf, myFunction as foo };
