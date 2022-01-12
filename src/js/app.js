48889f60f5d331ce199f004e92068a4a5f1ee558
roowix <gerasimenko_andrei@bk.ru>
2022-01-12 17:57:36 +0700

    project avia tickets part 1

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,11 @@ import Product from "./module2";
 console.log(new Product("Jija"));
 mod1.foo(); */
 
-import { getData } from "./module1";
+/* import { getData } from "./module1";
 
-console.log(getData());
+console.log(getData()); */
+
+import api from "./services/apiService";
+
+api.countries().then((res) => console.log(res));
+api.cities().then((res) => console.log(res));
