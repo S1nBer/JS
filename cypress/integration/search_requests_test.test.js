30e75bc8d37df1aec908867564d5d730411d9906
Andrei Gerasimenko <gerasimenko_andrei@bk.ru>
2022-05-12 17:26:30 +0700

    cypress commands

--- cypress/integration/search_requests_test.test.js
+++ cypress/integration/search_requests_test.test.js
@@ -1,21 +1,13 @@
+/* const { cy } = require("date-fns/locale"); */
+
 describe("Test search requests", () => {
-  it("visit homepage", () => {
-    cy.intercept("GET", "https://aviasales-api.herokuapp.com/countries", {
-      fixture: "countries.json",
-    }).as("getCountries");
-    cy.intercept("GET", "https://aviasales-api.herokuapp.com/cities", {
-      fixture: "cities.json",
-    }).as("getCities");
-    cy.intercept("GET", "https://aviasales-api.herokuapp.com/airlines", {
-      fixture: "airlines.json",
-    }).as("getAirlines");
+  before(() => {
+    cy.reachThePage();
+  });
 
-    cy.visit("http://localhost:9000");
-    cy.get("[data-hook=mainForm]").should("be.visible");
-
-    cy.wait("@getCountries");
-    cy.wait("@getCities");
-    cy.wait("@getAirlines");
+  beforeEach(() => {
+    cy.initElements();
+    cy.fillTheForm();
   });
 
   it("Form submit with correct request params", () => {
@@ -30,37 +22,6 @@ describe("Test search requests", () => {
         expect(req.query.destination).to.equal("PAR");
       }
     );
-
-    cy.get("[data-hook=autocompleteOrigin]").as("autocompleteOrigin");
-    cy.get("[data-hook=autocompleteDestination]").as("autocompleteDestination");
-    cy.get("[data-hook=datepickerDepartInput]").as("datepickerDepartInput");
-    cy.get("[data-hook=datepickerDepartWrap] .datepicker-container").as(
-      "modalWindow"
-    );
-    cy.get("[data-hook=submitButton]").as("submitButton");
-
-    cy.get("@autocompleteOrigin").type("Харьков");
-    cy.get(".field-origin .autocomplete-content li:first")
-      .contains("Харьков, Украина")
-      .click();
-    cy.get("@autocompleteDestination").type("Париж");
-    cy.get(".field-destination .autocomplete-content li:first")
-      .contains("Париж, Франция")
-      .click();
-
-    cy.get("@datepickerDepartInput").click();
-    cy.get("@modalWindow").should("be.visible");
-
-    cy.get(
-      "[data-hook=datepickerDepartWrap] .datepicker-container .is-today"
-    ).as("today");
-    cy.get(
-      "[data-hook=datepickerDepartWrap] .datepicker-container .btn-flat"
-    ).as("modalButtons");
-
-    cy.get("@today").click();
-    cy.get("@today").should("have.class", "is-selected");
-    cy.get("@modalButtons").contains("Ok").click();
 
     cy.get("@submitButton").click();
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,58 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("reachThePage", () => {
+  cy.intercept("GET", "https://aviasales-api.herokuapp.com/countries", {
+    fixture: "countries.json",
+  }).as("getCountries");
+  cy.intercept("GET", "https://aviasales-api.herokuapp.com/cities", {
+    fixture: "cities.json",
+  }).as("getCities");
+  cy.intercept("GET", "https://aviasales-api.herokuapp.com/airlines", {
+    fixture: "airlines.json",
+  }).as("getAirlines");
+
+  cy.visit("http://localhost:9000");
+
+  cy.wait("@getCountries");
+  cy.wait("@getCities");
+  cy.wait("@getAirlines");
+});
+
+Cypress.Commands.add("initElements", () => {
+  cy.get("[data-hook=autocompleteOrigin]").as("autocompleteOrigin");
+  cy.get("[data-hook=autocompleteDestination]").as("autocompleteDestination");
+  cy.get("[data-hook=datepickerDepartInput]").as("datepickerDepartInput");
+  cy.get("[data-hook=datepickerDepartWrap] .datepicker-container").as(
+    "modalWindow"
+  );
+  cy.get("[data-hook=submitButton]").as("submitButton");
+  cy.get("[data-hook=resetButton]").as("resetButton");
+});
+
+Cypress.Commands.add("fillTheForm", () => {
+  cy.get("@resetButton").click();
+
+  cy.get("@autocompleteOrigin").type("Харьков");
+  cy.get(".field-origin .autocomplete-content li:first")
+    .contains("Харьков, Украина")
+    .click();
+  cy.get("@autocompleteDestination").type("Париж");
+  cy.get(".field-destination .autocomplete-content li:first")
+    .contains("Париж, Франция")
+    .click();
+
+  cy.get("@datepickerDepartInput").click();
+  cy.get("@modalWindow").should("be.visible");
+
+  cy.get("[data-hook=datepickerDepartWrap] .datepicker-container .is-today").as(
+    "today"
+  );
+  cy.get("[data-hook=datepickerDepartWrap] .datepicker-container .btn-flat").as(
+    "modalButtons"
+  );
+
+  cy.get("@today").click();
+  cy.get("@today").should("have.class", "is-selected");
+  cy.get("@modalButtons").contains("Ok").click();
+});
